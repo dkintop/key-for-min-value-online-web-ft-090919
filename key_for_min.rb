cdafef02efdd30077b55624aa19c4501a2008d4a
Doug Kintop <dkintop@hotmail.com>
2019-09-13 17:29:40 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,8 +5,8 @@ def key_for_min_value(hash)
   smallest_key = nil
   smallest_value = nil
   hash.each do |key, value|
-    if tiniest_value == nil || value < tiniest_value
-      tiniest_value = value
+    if smallest_value == nil || value < smallest_value
+      smallest_value = value
       smallest_key = key
     end
   end
