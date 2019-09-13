17f0396e9d2d0305a4a1069e982d1180e39975ca
Doug Kintop <dkintop@hotmail.com>
2019-09-13 17:24:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,11 @@
 
 def key_for_min_value(name_hash)
 
+  smallest = placeholder
+  name_hash.each do |key, value|
+    if value < smallest || smallest == placeholder
+      smallest = key
+    end
+    smallest
+  end
 end
